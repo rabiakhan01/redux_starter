c477158806ee7ccc822eef911535d651246526be
rabiakhan01 <rabiakham789@gmail.com>
2024-06-26 16:52:47 +0500

    feat: maping of cart data

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -2,14 +2,14 @@ const images = {
 
     cart: require('./cart.png'),
 
-    almondMilk: require('./almon-milk.jpg'),
+    almondMilk: require('./almond-milk.jpg'),
     avocada: require('./avocada.jpg'),
     banana: require('./banana.jpg'),
     carrot: require('./carrots.jpg'),
     egg: require('./eggs.jpg'),
     apple: require('./organic-apple.jpg'),
-    colliflower: require('./colliflower.jpg'),
-    meat: require('./packed-meat.jpg'),
+    colliflower: require('./colliflower.png'),
+    meat: require('./meat.jpg'),
     spinach: require('./spinach.jpg'),
     strawberry: require('./strawberry.jpg')
 }
--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,26 +2,33 @@ import React from "react";
 import images from "../../assets/images/images";
 import { useDispatch } from "react-redux";
 import { addToCart, removeToCart } from "../../services/actions";
+import { allProducts } from "../../utils/dummyData";
 
 const Cart = () => {
     const dispatch = useDispatch();
     return (
-        <div>
-            <div className="flex justify-between items-center w-[40%] border border-gray-400 h-40 ml-4 px-2">
-                <img src={images.colliflower} alt="" className="object-cover h-20 w-20 rounded-md" />
-                <div>
-                    <p>Name: ColliFlower</p>
-                    <p>Price: $10</p>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <button className="bg-pink-500 px-2 py-2 rounded-md text-white" onClick={() => dispatch(addToCart({
-                        id: 1,
-                        name: 'Colliflower',
-                        price: '10'
-                    }))}>Add To Cart</button>
-                    <button className="bg-sky-500 px-2 py-2 rounded-md text-white" onClick={() => dispatch(removeToCart())}>Remove To Cart</button>
-                </div>
-            </div>
+        <div className="flex flex-col gap-3">
+            {
+                allProducts.map((item) => {
+                    return (
+                        <div className="flex justify-between items-center w-[40%] border border-gray-400 h-32 ml-4 px-4" key={item.id}>
+                            <img src={item.image} alt="" className="object-cover h-16 w-16 rounded-md" />
+                            <div>
+                                <p>Name: {item.name}</p>
+                                <p>Price: ${item.price}</p>
+                            </div>
+                            <div className="flex flex-col gap-3">
+                                <button className="bg-pink-500 px-2 py-2 rounded-md text-white" onClick={() => dispatch(addToCart({
+                                    id: 1,
+                                    name: 'Colliflower',
+                                    price: '10'
+                                }))}>Add To Cart</button>
+                                <button className="bg-sky-500 px-2 py-2 rounded-md text-white" onClick={() => dispatch(removeToCart())}>Remove To Cart</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
