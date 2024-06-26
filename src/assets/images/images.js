a3c7a5b0e96b074615dcb0ab02ef1914e12dbdd2
rabiakhan01 <rabiakham789@gmail.com>
2024-06-26 16:42:02 +0500

    feat: dummy data for the cart is created and images are exported from the aseets

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,6 +1,17 @@
 const images = {
-    colliflower: require('./colliflower.jpg'),
+
     cart: require('./cart.png'),
+
+    almondMilk: require('./almon-milk.jpg'),
+    avocada: require('./avocada.jpg'),
+    banana: require('./banana.jpg'),
+    carrot: require('./carrots.jpg'),
+    egg: require('./eggs.jpg'),
+    apple: require('./organic-apple.jpg'),
+    colliflower: require('./colliflower.jpg'),
+    meat: require('./packed-meat.jpg'),
+    spinach: require('./spinach.jpg'),
+    strawberry: require('./strawberry.jpg')
 }
 
 export default images;
---
+++ src/utils/dummyData/index.js
@@ -0,0 +1,63 @@
+import images from '../../assets/images/images';
+export const allProducts = [
+    {
+        "id": 1,
+        "name": "Organic Apples",
+        "price": 3.99,
+        "image": images.apple,
+    },
+    {
+        "id": 2,
+        "name": "Organic Banana",
+        "price": 2.49,
+        "image": images.banana,
+    },
+    {
+        "id": 3,
+        "name": "Almond Milk",
+        "price": 3.29,
+        "image": images.almondMilk,
+    },
+    {
+        "id": 4,
+        "name": "Free-Range Eggs",
+        "price": 4.99,
+        "image": images.egg,
+    },
+    {
+        "id": 5,
+        "name": "ColliFlower",
+        "price": 1.99,
+        "image": images.colliflower,
+    },
+    {
+        "id": 6,
+        "name": "Organic Carrots",
+        "price": 2.79,
+        "image": images.carrot,
+    },
+    {
+        "id": 7,
+        "name": "Organic Strawbery",
+        "price": 5.99,
+        "image": images.strawberry,
+    },
+    {
+        "id": 8,
+        "name": "Packed Meat",
+        "price": 6.49,
+        "image": images.meat,
+    },
+    {
+        "id": 9,
+        "name": "Avocado",
+        "price": 1.49,
+        "image": images.avocada,
+    },
+    {
+        "id": 10,
+        "name": "Organic Spinach",
+        "price": 2.99,
+        "image": images.spinach,
+    }
+]
