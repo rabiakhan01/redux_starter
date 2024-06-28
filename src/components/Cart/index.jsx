822adf59b3bbbf544cb3db1c66cbb83b1412e59e
rabiakhan01 <rabiakham789@gmail.com>
2024-06-28 15:42:55 +0500

    feat: combined reducer

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -8,10 +8,10 @@ const Cart = () => {
 
     //get data from the store
     const result = useSelector((state) => state.cartItems);
-    // console.log("🚀 ~ Cart ~ result:", result)
+    console.log("🚀 ~ Cart ~ result:", result)
 
     const res = useSelector((state) => state)
-    // console.log("🚀 ~ Cart ~ res:", res)
+    console.log("🚀 ~ state ~ result:", res)
     //call action when the user click on the cart button
     const handelAddToCart = (item) => {
         dispatch(addToCart({
--- src/redux/Information/reducer.js
+++ src/redux/Information/reducer.js
@@ -1,12 +1,9 @@
 import { ADD_INFORMATION } from "./types";
 
-const informationData = (state = [], action) => {
+const informationData = (state = {}, action) => {
     switch (action.type) {
         case ADD_INFORMATION:
-            return [
-                ...state,
-                action.data
-            ]
+            return action.data
         default:
             return state
     }
